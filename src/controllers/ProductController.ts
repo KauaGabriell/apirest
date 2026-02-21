0315b124e4160f2949da22d3165024bd274cb5b0
Kauã Gabriell <kauagabriell64@gmail.com>
2026-02-21 14:36:06 -0300

    Tratando Requisições

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -12,9 +12,15 @@ class ProductController {
     //Dessa forma passamos o middleware de forma local e o "next" fica responsável por "passar" a requisição para a próxima função.
     const { name, price, user_id } = req.body;
 
+    if (!name || !price) {
+      throw new AppError('Nome do produto e preço são obrigatórios');
+    }
+
     // throw new Error('Erro ao tentar criar um Produto'); //Simulação de Erro para aprendizado de tratamento de exceções
 
     // throw new AppError('Erro ao criar um Produto'); Simulação de erro com nossa classe de Erro.
+
+    res.status(201).json({ name, price, user_id });
   }
 }
 
