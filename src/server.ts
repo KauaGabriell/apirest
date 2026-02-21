c31cfcb18b6f8ec59beff18ff81500d7a32aa9f7
Kauã Gabriell <kauagabriell64@gmail.com>
2026-02-21 10:29:35 -0300

    Query Params

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,14 @@ const app = express();
 const PORT = 1111;
 
 app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
+  const { id } = req.params; //Route Params
   res.send(`Produto ${id}`);
+});
+
+app.get('/products', (req, res) => {
+  const { page, limit } = req.query; //Query Params
+
+  res.send(`Página ${page} de ${limit}`);
 });
 
 app.listen(PORT, () => {
